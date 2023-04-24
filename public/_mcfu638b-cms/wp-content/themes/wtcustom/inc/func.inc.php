b762290fd62dc175c5e01c31477b6af06efbb0a0
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-24 10:37:47 +0200

    back to jQuery

--- public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/func.inc.php
+++ public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/func.inc.php
@@ -133,7 +133,7 @@ function customBackendScripts() {
             // customizeCarbonFieldsPlugin();
             customizeNestedPagesPlugin();
             flushSimplePagesCacheOnDrag();
-            // jQuery('.cf-complex__inserter-button').css('backgroundColor', '#b3edb3');
+            jQuery('.cf-complex__inserter-button').css('backgroundColor', '#b3edb3');
             // jQuery('.cf-complex__group-actions.cf-complex__group-actions--tabbed').css('backgroundColor', '#fde6e6');
 
             // function customizeCarbonFieldsPlugin() {
