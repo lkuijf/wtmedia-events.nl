f16e5f8658092d5aa1207b9e9f2903612a80d4ab
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-24 10:15:37 +0200

    echoing the styles

--- public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/func.inc.php
+++ public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/func.inc.php
@@ -36,17 +36,13 @@ function remove_page_row_actions($actions, $post) {
     return $actions;
 }
 function customBackendStyles() {
-    ?>
-    <style type="text/css">
-      /* #taxonomy-category #taxonomy-category-new { display: none; }
-      .cf-complex__inserter-button {
-          border: 1px solid red;
-      } */
+    echo '
+    <style>
       .cf-complex__group-actions.cf-complex__group-actions--tabbed {
         background-color: #fde6e6;
       }
     </style>
-    <?php
+    ';
 }
 function removePageActionsEditorRole() {
     ?>
