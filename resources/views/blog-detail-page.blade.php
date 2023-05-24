e72137b4b6feb28fffcc85dd566fcdd6a8e34b00
Leon Kuijf <leon.kuijf@gmail.com>
2023-05-24 08:53:50 +0200

    blog title section

--- resources/views/blog-detail-page.blade.php
+++ resources/views/blog-detail-page.blade.php
@@ -1,4 +1,6 @@
 @extends('templates.wtme')
+@section('page_title', $data['head_title'])
+@section('meta_description', $data['meta_description'])
 @section('content')
     {{-- @include('snippets.contentSections') --}}
     <div class="standardPage">
