081c9def710f2bc3baf4def3e6cb7436ee54c121
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-05 11:20:09 +0200

    multiple cats

--- public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/callbacks.inc.php
+++ public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/callbacks.inc.php
@@ -241,10 +241,11 @@ function getCustomPostsCollectionAttrs($coll) {
         $catTerms = get_the_terms( $item->ID, 'case_category' );
         if($catTerms && count($catTerms)) {
             foreach($catTerms as $term) {
-                $oCat = new stdClas();//
-                $oCat->slug = $term->slug;
-                $oCat->name = $term->name;
-                $cats[] = $oCat;
+                // $oCat = new stdClas();
+                $sCat = [];
+                $sCat['slug'] = $term->slug;
+                $sCat['name'] = $term->name;
+                $cats[] = $sCat;
             }
         }
 
