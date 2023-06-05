a52c10d2b440c08ac5dfdfd5beea87b13fc94561
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-05 09:59:40 +0200

    clear &highlighted=1

--- public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/func.inc.php
+++ public/_mcfu638b-cms/wp-content/themes/wtcustom/inc/func.inc.php
@@ -32,6 +32,7 @@ function deleteSimpleCustomPostsRestCacheBlog() {
 }
 function deleteSimpleCustomPostsRestCacheCase() {
     \WP_Rest_Cache_Plugin\Includes\Caching\Caching::get_instance()->delete_cache_by_endpoint( '/_mcfu638b-cms/index.php/wp-json/wtcustom/simple-custom-posts?post_type=case' );
+    \WP_Rest_Cache_Plugin\Includes\Caching\Caching::get_instance()->delete_cache_by_endpoint( '/_mcfu638b-cms/index.php/wp-json/wtcustom/simple-custom-posts?post_type=case&highlighted=1' );
     \WP_Rest_Cache_Plugin\Includes\Caching\Caching::get_instance()->delete_cache_by_endpoint( '/_mcfu638b-cms/index.php/wp-json/wtcustom/simple-custom-posts?category=online-marketing&post_type=case' );
     \WP_Rest_Cache_Plugin\Includes\Caching\Caching::get_instance()->delete_cache_by_endpoint( '/_mcfu638b-cms/index.php/wp-json/wtcustom/simple-custom-posts?category=web-development&post_type=case' );
     \WP_Rest_Cache_Plugin\Includes\Caching\Caching::get_instance()->delete_cache_by_endpoint( '/_mcfu638b-cms/index.php/wp-json/wtcustom/simple-custom-posts?category=events&post_type=case' );
