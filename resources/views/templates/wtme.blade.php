4d11a0b86b070b1f3e88978f29444fc766583b92
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-05 15:00:03 +0200

    Leadinfo tracking code

--- resources/views/templates/wtme.blade.php
+++ resources/views/templates/wtme.blade.php
@@ -20,6 +20,13 @@
     <link rel="stylesheet" href="{{ asset('css/swiper-bundle.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @yield('extra_head')
+    
+<!-- Leadinfo tracking code -->
+<script> (function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
+l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;
+l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}
+}(window,document,"script","https://cdn.leadinfo.net/ping.js","leadinfo","LI-6479E387ADE15")); </script>
+
 </head>
 <body>
     @yield('after_body_tag')
