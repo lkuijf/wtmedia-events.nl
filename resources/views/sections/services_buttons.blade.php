7c49cd9d00cfbcf30b97724bf6df238004a65fc1
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-14 12:06:11 +0200

    static button webdeveloment

--- resources/views/sections/services_buttons.blade.php
+++ resources/views/sections/services_buttons.blade.php
@@ -1,5 +1,5 @@
 <div class="servicesBtns">
     <p class="serviceBtnWrap"><a href="{{ url('diensten/online-marketing') }}" class="serOM">Online Marketing</a></p>
-    <p class="serviceBtnWrap"><a href="{{ url('diensten/web-development') }}" class="serWD">Web Development</a></p>
+    <p class="serviceBtnWrap"><a href="{{ url('diensten/webdevelopment') }}" class="serWD">Webdevelopment</a></p>
     <p class="serviceBtnWrap"><a href="{{ url('diensten/events') }}" class="serEV">Events</a></p>
 </div>
