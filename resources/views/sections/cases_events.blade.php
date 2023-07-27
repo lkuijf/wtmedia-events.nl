65d5e2a81516239033e1e634fed18298e2952f8c
Leon Kuijf <leon.kuijf@gmail.com>
2023-07-27 15:45:00 +0200

    web dev gallery

--- resources/views/sections/cases_events.blade.php
+++ resources/views/sections/cases_events.blade.php
@@ -1,8 +1,6 @@
 <div class="inner">
-    {{-- <div class="eventRelativeWrap"> --}}
-        <div class="casesContentDiensten eventCases">
-            @include('sections.cases', ['cases' => $cases, 'type' => 'events'])
-        </div>
-        <p class="toggleEvents"><a href="">Bekijk meer</a></p>
-    {{-- </div> --}}
+    <div class="casesContentDiensten eventCases">
+        @include('sections.cases', ['cases' => $cases, 'type' => 'events'])
+    </div>
+    <p class="toggleEvents"><a href="">Bekijk meer</a></p>
 </div>
--- resources/views/sections/cases_web_development.blade.php
+++ resources/views/sections/cases_web_development.blade.php
@@ -1,5 +1,30 @@
 <div class="inner">
-    <div class="casesContentDiensten webDevelopmentCases">
-        @include('sections.cases', ['cases' => $cases, 'type' => 'web-development'])
+    {{-- <div class="casesContentDiensten webDevelopmentCases"> --}}
+        {{-- @include('sections.cases', ['cases' => $cases, 'type' => 'web-development']) --}}
+    {{-- </div> --}}
+
+    <div class="webDevCasesContent">
+        <div class="webDevNumbers">
+            <div>
+                <span>234</span>
+                <span>happy clients</span>
+            </div>
+            <div>
+                <span>12</span>
+                <span>projects</span>
+            </div>
+        </div>
+        <div class="webDevGallery">
+            @include('sections.cases', ['cases' => $cases, 'type' => 'web-development'])
+        </div>
     </div>
+
+    {{-- <div class="webDevCasesContent">
+        <div class="webDevNumber"><div>234</div></div>
+        <div class="webDevNumber"><div>12</div></div>
+        <div class="webDevGallery">
+            @include('sections.cases', ['cases' => $cases, 'type' => 'web-development'])
+        </div>
+    </div> --}}
+    
 </div>
