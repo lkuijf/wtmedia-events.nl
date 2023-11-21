7c80468344f7006c4b40654fe7c351b4a46fb6f8
Leon Kuijf <leon.kuijf@gmail.com>
2023-11-21 16:40:49 +0100

    team member cards finished

--- resources/views/sections/team_specialists.blade.php
+++ resources/views/sections/team_specialists.blade.php
@@ -5,10 +5,9 @@
             <div><img src="{!! $teamMember->image[0]['sizes']['medium_large'] !!}" alt="{{ $teamMember->image[0]['alt'] }}"></div>
             <div>
                 <h3>{{ $teamMember->title->rendered }}</h3>
-                <p>{{ $teamMember->function }}</p>
-                {{-- {!! $teamMember->text !!} --}}
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi. Rutrum quisque non tellus orci ac auctor augue mauris augue.</p>
-                <p><a href="">Lees meer</a></p>
+                <p class="tmFunc">{{ $teamMember->function }}</p>
+                <p>{{ $teamMember->card_text }}</p>
+                <p><a href="{{ url('over-wt-media-events#') }}{{ $teamMember->slug }}">Lees meer</a></p>
             </div>
         </article>
         @endforeach
