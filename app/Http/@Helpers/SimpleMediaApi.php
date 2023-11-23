2f3b755fa9a77b22ccc94cacbe5957a45cd82ffd
Leon Kuijf <leon.kuijf@gmail.com>
2023-11-23 15:09:09 +0100

    helpers from wtt

--- app/Http/@Helpers/SimpleMediaApi.php
+++ app/Http/@Helpers/SimpleMediaApi.php
@@ -3,7 +3,7 @@ namespace App\Http\Helpers;
 
 class SimpleMediaApi extends ApiCall {
     public function __construct() {
-        // $this->endpoint = config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/simple-media';
+        // $this->endpoint = '/index.php/wp-json/wtcustom/simple-media';
         $this->endpoint = '/wtcustom/simple-media';
     }
     public function makeListById() {
--- app/Http/Helpers/CustomPostApi.php
+++ app/Http/Helpers/CustomPostApi.php
@@ -3,8 +3,14 @@ namespace App\Http\Helpers;
 
 class CustomPostApi extends ApiCall {
     public function __construct($customPostType, $id = false, $slug = false) {
-        $this->endpoint = '/index.php/wp-json/wp/v2/' . $customPostType;
-        if($id)     $this->endpoint = '/index.php/wp-json/wp/v2/' . $customPostType . '/' . $id;
-        if($slug)   $this->endpoint = '/index.php/wp-json/wp/v2/' . $customPostType . '?slug=' . $slug;
+        $this->endpoint = config('app_wt.cmsPath') . '/index.php/wp-json/wp/v2/' . $customPostType;
+        if($id !== false) {
+            if(is_array($id)) { // multiple id's
+                $this->endpoint = config('app_wt.cmsPath') . '/index.php/wp-json/wp/v2/' . $customPostType . '?include=' . implode(',', $id);
+            } else {
+                $this->endpoint = config('app_wt.cmsPath') . '/index.php/wp-json/wp/v2/' . $customPostType . '/' . $id;
+            }
+        }
+        if($slug)   $this->endpoint = config('app_wt.cmsPath') . '/index.php/wp-json/wp/v2/' . $customPostType . '?slug=' . $slug;
     }
 }
